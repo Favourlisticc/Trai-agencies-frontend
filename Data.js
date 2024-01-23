76a4dfc8775b0f03cfb4bb08d8fc8ab88ff51a75
Lomoee <favoursunday600@gmail.com>
2024-01-23 09:51:22 +0100

    temp data

---
+++ Data.js
@@ -0,0 +1,53 @@
+export const activitiesdata = [
+    {
+        id: 1,
+        companyName: "Cromwell Medical",
+        logo: "https://www.cromwellmedical.com/gfx/sec_logo.svg",
+
+        text: "Education"
+    },
+    {
+        id: 2,
+        companyName: "Daniel Owen",
+        logo: "https://www.danielowen.co.uk/uploads/do-logo.svg",
+
+        text: "Health Aid"
+    },
+    {
+        id: 3,
+        companyName: "In 2 Ed Education",
+        logo: "https://static.wixstatic.com/media/39a349_34d97c2709484f5ca5a8428b2565aecf~mv2.png/v1/crop/x_758,y_0,w_3561,h_3740/fill/w_94,h_98,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Original%20on%20Transparent.png",
+        text: "Poverty Relief"
+    },
+    {
+        id: 4,
+        companyName: "MCG Construction",
+        logo: "https://www.mcgconstruction.co.uk/style/images/mcg-construct-logoo.svg",
+        text: "Child Welfare"
+    },
+    {
+        id: 5,
+        companyName: "Orbis",
+        logo: "",
+
+        text: "Human Rights"
+    },
+    {
+        id: 6,
+        companyName: "Prospect Health",
+        logo: "https://www.prospect-health.com/wp-content/themes/prospecthealth/Logo-Prospect-Health.png",
+        text: "Disaster Relief"
+    },
+    {
+        id: 7,
+        companyName: "PSR Solutions",
+        logo: "",
+        text: "Disaster Relief"
+    },
+    {
+        id: 8,
+        companyName: "PSR Solutions",
+        logo: "",
+        text: "Disaster Relief"
+    },
+];export default activitiesdata;
