126926234d1a3e1fa23da644993b65f339465013
Lomoee <favoursunday600@gmail.com>
2024-01-23 10:11:22 +0100

    data

--- Data.js
+++ Data.js
@@ -3,51 +3,117 @@ export const activitiesdata = [
         id: 1,
         companyName: "Cromwell Medical",
         logo: "https://www.cromwellmedical.com/gfx/sec_logo.svg",
+        Address: "3 More London Riverside",
 
-        text: "Education"
+        Website: "https://www.cromwellmedical.com/",
+                socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
+
+
     },
     {
         id: 2,
         companyName: "Daniel Owen",
         logo: "https://www.danielowen.co.uk/uploads/do-logo.svg",
-
-        text: "Health Aid"
+        Address: "1st Floor Genesis House 17 Godliman Street London",
+        
+        Website: "https://www.danielowen.co.uk/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
     },
+
     {
         id: 3,
         companyName: "In 2 Ed Education",
         logo: "https://static.wixstatic.com/media/39a349_34d97c2709484f5ca5a8428b2565aecf~mv2.png/v1/crop/x_758,y_0,w_3561,h_3740/fill/w_94,h_98,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Original%20on%20Transparent.png",
-        text: "Poverty Relief"
+        Address: "181 Holloway Rd, London N7 8LX, United Kingdom",
+    
+        Website: "https://www.in2ed.net/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
     },
     {
         id: 4,
         companyName: "MCG Construction",
         logo: "https://www.mcgconstruction.co.uk/style/images/mcg-construct-logoo.svg",
-        text: "Child Welfare"
+        Address: "No 1 Poultry, London, EC2R 8EJ",
+        Website: "https://www.mcgconstruction.co.uk/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+
+
     },
     {
         id: 5,
         companyName: "Orbis",
         logo: "",
 
-        text: "Human Rights"
+        Address: "WeWork, 77 Leadenhall St",
+
+        Website: "https://www.weareorbis.com/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
     },
     {
         id: 6,
         companyName: "Prospect Health",
         logo: "https://www.prospect-health.com/wp-content/themes/prospecthealth/Logo-Prospect-Health.png",
-        text: "Disaster Relief"
+        Address: "Clarendon House, 2nd Floor, Victoria Ave, Harrogate HG1 1JD",
+        
+        Website: "https://www.prospect-health.com/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
+
+
     },
     {
         id: 7,
         companyName: "PSR Solutions",
         logo: "",
-        text: "Disaster Relief"
+        Address: " 3rd Floor, 38 Charterhouse Street, Barbican, London EC1M 6JN",
+        
+        Website: "https://www.psrsolutions.co.uk/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
+    
     },
     {
         id: 8,
         companyName: "PSR Solutions",
         logo: "",
-        text: "Disaster Relief"
+        Address: "",
+
+        Website: "https://www.randstad.com/",
+        socialmedia :[
+            linkedin= "",
+            youtube= ""
+        ]
+        
+
     },
-];export default activitiesdata;
+]
+;export default activitiesdata;
