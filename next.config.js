69f28934de3fd7078f4cb964884afb3ba366ee83
Lomoee <favoursunday600@gmail.com>
2024-01-24 11:44:53 +0100

    deploy-error-solved-2.0

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 }
-
-
-
+ 
 module.exports = nextConfig
